b618f68b2ec08573d0284a9494b546ef89fa5df0
tomohisa2012 <20190363@school.kasugai.ed.jp>
2026-02-11 21:38:42 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+　
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("clock-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
